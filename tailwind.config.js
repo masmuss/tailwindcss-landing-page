8b8884247effec735c5f2f93b028ad38820bff56
A. Musafir Khoirul F <ahmusafir@it.student.pens.ac.id>
2021-11-12 14:13:20 +0700

    create navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      OpenSans: ["Open Sans, sans-serif"]
+    },
+    container: {
+      center: true,
+      padding: "1rem"
+    }
   },
   variants: {
     extend: {},
